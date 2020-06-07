4acbb797f180a0a330066e7f31d0b210d28c362d
Madhu5A3 <madhuv299@gmail.com>
2020-06-07 10:27:06 +0530

    Update: Router Push Experimentation

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
 import Layout from '../components/Layout';
-
+import Router from 'next/router';
 const About = () => {
     return (
         <Layout>
             <div>
                 Welcome to About Page
+                <button onClick={() => Router.push('/')}>GO BACK TO HOME PAGE</button>
             </div>
         </Layout>
     );
