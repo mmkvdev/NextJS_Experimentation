3ad847e11da0f036939f19c3477029ffce2cc0e1
Madhu5A3 <madhuv299@gmail.com>
2020-06-07 08:31:48 +0530

    Update: Conditionals for Currency Selection

--- components/Prices.js
+++ components/Prices.js
@@ -1,17 +1,40 @@
 import { useState } from 'react';
 
 const Prices = (props) => {
-    const [currency, updateCurrency] = useState('USD')
+    const [currency, updateCurrency] = useState('GBP')
+
+    let currencyList = '';
+    if (currency === 'USD') {
+        currencyList = <ul className="list-group">
+            <li className="list-group-item">
+                BitCoin rate for {props.bpi.USD.description} : <span className="badge badge-primary">
+                    {props.bpi.USD.code}
+                </span>
+                <strong>{props.bpi.USD.rate}</strong>
+            </li>
+        </ul>
+    } else if (currency === 'EUR') {
+        currencyList = <ul className="list-group">
+            <li className="list-group-item">
+                BitCoin rate for {props.bpi.EUR.description} : <span className="badge badge-primary">
+                    {props.bpi.EUR.code}
+                </span>
+                <strong>{props.bpi.EUR.rate}</strong>
+            </li>
+        </ul>
+    } else if (currency === 'GBP') {
+        currencyList = <ul className="list-group">
+            <li className="list-group-item">
+                BitCoin rate for {props.bpi.GBP.description} : <span className="badge badge-primary">
+                    {props.bpi.GBP.code}
+                </span>
+                <strong>{props.bpi.GBP.rate}</strong>
+            </li>
+        </ul>
+    }
     return (
         <div>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    BitCoin rate for {props.bpi.USD.description} : <span className="badge badge-primary">
-                        {props.bpi.USD.code}
-                    </span>
-                    <strong>{props.bpi.USD.rate}</strong>
-                </li>
-            </ul>
+            {currencyList}
         </div>
     );
 }
