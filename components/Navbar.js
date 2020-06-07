53cad9efeb2eeb3d783f0d0668dcd4a1f3fefba8
Madhu5A3 <madhuv299@gmail.com>
2020-06-07 06:58:38 +0530

    Update: Boostrapified the Layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,36 +2,21 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        <a>About</a>
-                    </Link>
-                </li>
-            </ul>
-            <style jsx>{`
-                ul li a {
-                    text-decoration: none;
-                    color: #fff;
-                }
-                ul {
-                    background: #333;
-                    color: #fff;
-                    list-style: none;
-                    display: flex;
-                }
-                ul li {
-                    font-size: 18px;
-                    margin-right: 20px;
-                }
-            `}</style>
-        </div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
+            <div className="container">
+                <a className="navbar-brand" href="#">Experimental Thymesia</a>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Home</a></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/about"><a className="nav-link">About</a></Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
