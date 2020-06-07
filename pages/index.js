9df42a5ff782858b20cb0780d61ef43f215a0322
Madhu5A3 <madhuv299@gmail.com>
2020-06-07 07:20:23 +0530

    Update: Isomorphic Fetch Updated

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,25 @@ import ClientSideRendering from './ClientSideRendering/ClientSideRendering';
 // import Link from 'next/link';
 // import Navbar from '../components/Navbar';
 import Layout from '../components/Layout';
+import Fetch from 'isomorphic-unfetch';
 
-function IndexApp() {
+function IndexApp(props) {
   return (
     <Layout>
       <div>Welcome To Home Page!!!
+        {props.bpi.time.updated}
         <ClientSideRendering />
       </div>
     </Layout>
   );
+}
+
+IndexApp.getInitialProps = async function () {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+  return {
+    bpi: data
+  };
 }
 
 export default IndexApp;
